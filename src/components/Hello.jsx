9ab77462c34411e6a82fb66c01cc62a04f8c37ac
yohanesarmenian <yohanesarmenian7@gmail.com>
2021-12-12 13:18:15 +0700

    first commit

---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+const Hello = () => {
+    return(
+        <>
+        <h1>Hello from hello component!</h1>
+        </>
+    )
+}
+
+export default Hello
+
+//Menggunakan Functional component
---
+++ src/components/HelloAgain.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react"
+
+export default class HelloAgain extends Component {
+    render() {
+        return(
+            <h1> Hello from class component</h1>
+        )
+    }
+}
+
+// menggunakan class component
